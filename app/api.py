a37adf5245938cc7dec5584b9c83454f13b3f985
Sandesh Thakar <112849863+sandesh-mewt@users.noreply.github.com>
2023-02-03 15:54:16 +0530

    code comments

--- app/api.py
+++ app/api.py
@@ -29,10 +29,13 @@ def upload_to_s3(df, file_name):
 def get_uptime_and_downtime(
     store_hours_list: List[Tuple[datetime, datetime]], store_status_intervals: List[Tuple[datetime, datetime, str]]
 ) -> Tuple[int, int]:
+
+    # List to store the overlapping intervals between store hours and store status intervals
     overlaps = []
 
     i = j = 0
 
+    # Iterate over both lists to find overlapping intervals
     while i < len(store_hours_list) and j < len(store_status_intervals):
         start1 = store_hours_list[i][0]
         end1 = store_hours_list[i][1]
@@ -40,17 +43,24 @@ def get_uptime_and_downtime(
         start2 = store_status_intervals[j][0]
         end2 = store_status_intervals[j][1]
 
+        # Find the maximum start time of the two intervals
         max_start = max(start1, start2)
+        # Find the minimum end time of the two intervals
         min_end = min(end1, end2)
+
+        # If there is an overlap between the two intervals, add it to the `overlaps` list
         if max_start < min_end:
             overlaps.append((max_start, min_end, store_status_intervals[j][2]))
+        # If the end of the store hours interval is before the end of the store status interval,
+        # move to the next store hours interval
         if end1 < end2:
             i += 1
         else:
+            # Otherwise, move to the next store status interval
             j += 1
 
     uptime = downtime = 0
-
+    # Iterate over the overlapping intervals and compute uptime and downtime
     for interval in overlaps:
         delta = interval[1] - interval[0]
         hours = delta.total_seconds() / 3600
@@ -65,6 +75,7 @@ def get_uptime_and_downtime(
     return uptime, downtime
 
 
+# TO-DO - Modularize the code for calculating store hours intervals
 def get_store_hours_intervals():
     pass
 
@@ -73,7 +84,8 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
     UTC_TIMEZONE = pytz.timezone("UTC")
     DEFAULT_TIMEZONE = "America/Chicago"
 
-    current_time = datetime(2023, 1, 25, 18, 0, 0, tzinfo=UTC_TIMEZONE)  # Assumed a static current time for the demo
+    # Create datetime objects for the current time, 1 hour ago, 1 day ago and 1 week ago
+    current_time = datetime(2023, 1, 25, 18, 0, 0, tzinfo=UTC_TIMEZONE)
     last_hour = current_time - timedelta(hours=1)
     last_day = current_time - timedelta(days=1)
     last_week = current_time - timedelta(days=7)
@@ -98,17 +110,20 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
     current_day = current_time_of_store_time_zone.weekday()
 
     # Create a dictionary to store all the days for the last week
-
-    # Example - current day is wednesay 25
+    # Example - current day is wednesday 25
     # week_datetimes[2] will be datetime object with date 25
-
     week_datetimes = {}
 
+    # Loop through 7 days (last week)
     for i in range(7):
+        # Calculate the day by subtracting the number of days from the current day
+        # using the modulo operator to ensure it stays within a range of 0-6 (days of the week)
         day_datetime = current_time_of_store_time_zone - timedelta(days=(current_day - i + 7) % 7)
+        # Get the weekday as an integer, where Monday is 0 and Sunday is 6
         day = day_datetime.weekday()
 
         # Add the datetime to the dictionary
+        # using the weekday as the key, so that the week_datetimes[2] will return the datetime object for Wednesday
         week_datetimes[day] = day_datetime
 
     # Get the store hours for the store
@@ -123,13 +138,16 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
     # If store hours are not found assume it to be open 24 * 7
     if not store_hours:
         for day in range(7):
+            # Calculate the start time of each day as midnight in UTC timezone
             start_time = (
                 week_datetimes[day].replace(hour=0, minute=0, second=0, microsecond=0).astimezone(UTC_TIMEZONE)
             )
+            # Calculate the end time of each day as midnight of the next day in UTC timezone
             end_time = (
                 week_datetimes[day].replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
             ).astimezone(UTC_TIMEZONE)
             store_hours_list.append((start_time, end_time))
+    # If store hours are found, loop through each store hours interval
     else:
         for item in store_hours:
             day = item.day
@@ -168,10 +186,8 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
     )
 
     # Make intervals by extrapolating the observed datapoints
-
     # Example if 25 Jan 17:10 was active and the next inactive observation was at 25 Jan 20:02 then
     # 25 Jan 17:10 to 25 Jan 20:02 will be considered as an active time interval
-
     store_status_intervals = []
 
     for i in range(1, len(store_status_in_last_hour)):
@@ -220,17 +236,8 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
 
             start_idx = i
             curr_status = store_status_in_last_day[i].status
-        
-        i += 1
 
-    # for i in range(1, len(store_status_in_last_day)):
-    #     store_status_intervals.append(
-    #         (
-    #             max(last_day, store_status_in_last_day[i - 1].timestamp_utc.replace(tzinfo=UTC_TIMEZONE)),
-    #             min(current_time, store_status_in_last_day[i].timestamp_utc.replace(tzinfo=UTC_TIMEZONE)),
-    #             store_status_in_last_day[i - 1].status,
-    #         )
-    #     )
+        i += 1
 
     uptime_last_day, downtime_last_day = get_uptime_and_downtime(store_hours_list, store_status_intervals)
 
@@ -269,17 +276,8 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
 
             start_idx = i
             curr_status = store_status_in_last_week[i].status
-        
-        i += 1
 
-    # for i in range(1, len(store_status_in_last_week)):
-    #     store_status_intervals.append(
-    #         (
-    #             max(last_week, store_status_in_last_week[i - 1].timestamp_utc.replace(tzinfo=UTC_TIMEZONE)),
-    #             min(current_time, store_status_in_last_week[i].timestamp_utc.replace(tzinfo=UTC_TIMEZONE)),
-    #             store_status_in_last_week[i - 1].status,
-    #         )
-    #     )
+        i += 1
 
     uptime_last_week, downtime_last_week = get_uptime_and_downtime(store_hours_list, store_status_intervals)
 
@@ -295,55 +293,37 @@ def process_store_data(store_id: str, db: Session) -> Tuple[str, int, int, int, 
 
 
 def generate_report(report: Report, db: Session) -> None:
-    # try:
-    # Create a list to store all unique store ids
-    import time
+    try:
 
-    start_time = time.time()
+        store_ids = [item.store_id for item in db.query(StoreStatus).distinct(StoreStatus.store_id).all()]
 
-    store_ids = [item.store_id for item in db.query(StoreStatus).distinct(StoreStatus.store_id).all()]
+        # List of lists to store data of all stores
+        report_list = [process_store_data(store_id, db) for store_id in store_ids]
 
-    print(len(store_ids))
+        report_df_columns = [
+            "store_id",
+            "uptime_last_hour",
+            "uptime_last_day",
+            "uptime_last_week",
+            "downtime_last_hour",
+            "downtime_last_day",
+            "downtime_last_week",
+        ]
+        report_df = pd.DataFrame(report_list, columns=report_df_columns)
 
-    store_ids = store_ids[0:250]
+        report_filename = report.report_id + ".csv"
 
-    # List of lists to store data of all stores
-    #report_list = [process_store_data(store_id, db) for store_id in store_ids]
+        report_url = upload_to_s3(report_df, report_filename)
 
-    report_list = []
-    for i in range(len(store_ids)):
-        print(i, store_ids[i])
-        report_list.append(process_store_data(store_ids[i], db))
-
-
-    end_time = time.time()
-
-    print((end_time - start_time) / 60)
-
-    report_df_columns = [
-        "store_id",
-        "uptime_last_hour",
-        "uptime_last_day",
-        "uptime_last_week",
-        "downtime_last_hour",
-        "downtime_last_day",
-        "downtime_last_week",
-    ]
-    report_df = pd.DataFrame(report_list, columns=report_df_columns)
-
-    report_filename = report.report_id + ".csv"
-
-    report_url = upload_to_s3(report_df, report_filename)
-
-    report.status = "Complete"
-    report.report = report_url
-    db.commit()
+        report.status = "Complete"
+        report.report = report_url
+        db.commit()
 
     # Set status to failed is any error occurs during report generation
-    # except Exception as ex:
-    #     print(repr(ex))
-    #     report.status = "Failed"
-    #     db.commit()
+    except Exception as ex:
+        print(repr(ex))
+        report.status = "Failed"
+        db.commit()
 
     return
 
